f7dc9c6ccc77d02ee31d7f228b5b7a6d85c6ba50
Vivek Sharma <viveksharmaspace786@gmail.com>
2020-12-02 03:56:20 +0530

    vivek

--- sharma.cpp
+++ sharma.cpp
@@ -3,10 +3,11 @@ using namespace std;
  int fun(int a,int b)
  return a*b
 int main()
-{
+{  
     int a;
     cin>>a;
     cout<<a*5;
     cout<<"this is the most authentic";
     cout<<fun(4,6);
+    cout<<"test";
 }
