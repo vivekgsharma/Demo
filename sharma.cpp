0c2e0b12191ed05c0619bced55f80199712a3e5c
Vivek Sharma <viveksharmaspace786@gmail.com>
2020-12-02 03:53:01 +0530

    second change

--- sharma.cpp
+++ sharma.cpp
@@ -1,10 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ int fun(int a,int b)
+ return a*b
 int main()
 {
     int a;
     cin>>a;
     cout<<a*5;
     cout<<"this is the most authentic";
+    cout<<fun(4,6);
 }
